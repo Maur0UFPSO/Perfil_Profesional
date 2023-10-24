4c0a4a93fc7766ef8e91de1f97583f736a15f24a
Maur0UFPSO <m30135807@gmail.com>
2023-10-24 14:00:57 -0500

    Exp_js

---
+++ js/funtions.js
@@ -0,0 +1,53 @@
+// tipos de variables 
+
+// const para constante 
+const pi=3.1416;
+const pul=2.54;
+// var para variables globales
+var cont_day= 10;
+var name='Mauricio';
+// let para variables locales
+let counter=0;
+let i = 1;
+
+// ################Formas de Impresion#########################
+alert('Impresion');
+console.log (counter);
+
+// bodypantalla
+
+document.write(counter);
+document.getElementById('text_one').innerHTML = "<h1>Hola Mundo</h1>"
+document.getElementById('text_two').innerText = "<h1>Hola Mundo</h1>"
+
+// https://sweetalert2.github.io/
+Swal.fire({
+    icon: 'success',
+    title: 'Oops...',
+    text: 'JEJEJEJEJEJEJEJE',
+    footer: 'y si..........?'
+  })
+
+  //############### Tipo de datos################
+  //numericos 
+  let number_one =10;
+  let number_two = 5.5;
+  //
+  let text ="soy un texto";
+  //
+  let boolean = true;
+  let boolean_2 = false;
+  //
+  let array_num = [1,2,3,4,5];
+  let array_text = ["juan", "maria" , "Jose Carlos"];
+  let array_mix = ["juan",1 , "Jose Carlos", 2];
+  let  array_mul = [
+    {name:Juan ,last_name: perez  ,age:12},
+    {name:Kevin ,last_name:perez  ,age:35},
+    {name:Jose ,last_name: perez ,age:63},
+    {name:David ,last_name:perez  ,age:4}
+  ]
+  //################################
+  var nombre_de_la_variable = "operaciones";
+  //############################################################ESTUDIAR METODOS##########################################
+  
